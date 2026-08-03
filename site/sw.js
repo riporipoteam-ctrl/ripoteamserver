ac8a74812d51f36174ca32ee493e7abb82c2ba4a
riporipoteam-ctrl <ripo.ripoteam@gmail.com>
2026-08-03 19:15:38 +0200

    Add resilient mobile site cache

---
+++ site/sw.js
@@ -0,0 +1,13 @@
+"use strict";
+const CACHE = "ripo-cloud-shell-v3";
+const CORE = ["./", "./index.html", "./styles.css", "./app.js", "./config.js", "./manifest.webmanifest", "./icon.svg"];
+self.addEventListener("install", (event) => { event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting())); });
+self.addEventListener("activate", (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())); });
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html"))));
+});
