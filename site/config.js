86bf7e02b30a44059d70aa7a50c8d061d7e93686
riporipoteam-ctrl <ripo.ripoteam@gmail.com>
2026-08-03 10:16:24 +0200

    Force fresh GitHub Pages deployment

--- site/config.js
+++ site/config.js
@@ -2,5 +2,6 @@ window.RIPO_CONFIG = {
   spaceUrl: "https://echoxr-ripoteam-cloud-pc.hf.space",
   cloudflareProxyUrl: "",
   pollSeconds: 5,
-  wakeTimeoutSeconds: 300
+  wakeTimeoutSeconds: 300,
+  deploymentVersion: "2026-08-03-novnc-fix-1"
 };
