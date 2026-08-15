bfba5729652ec42a2cb6ee05aaaafd6bd30f9e4b
riporipoteam-ctrl <ripo.ripoteam@gmail.com>
2026-08-15 16:11:44 +0200

    Install Rec Room session broker on deployed server

--- hf-space/app_server_v2.py
+++ hf-space/app_server_v2.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from app_server import DATA_DIR, DISPLAY, SERVER_TIKTOK_CONNECT, TIKTOK_AI, app, authorize
+from recroom_broker import install_recroom_broker_routes
 from server_live_broadcaster import ServerLiveBroadcaster, install_server_live_routes
 
 SERVER_LIVE_BROADCASTER = ServerLiveBroadcaster(
@@ -11,3 +12,5 @@ SERVER_LIVE_BROADCASTER = ServerLiveBroadcaster(
     DISPLAY,
 )
 install_server_live_routes(app, SERVER_LIVE_BROADCASTER)
+
+RECROOM_BROKER = install_recroom_broker_routes(app, DATA_DIR / "recroom-broker")
